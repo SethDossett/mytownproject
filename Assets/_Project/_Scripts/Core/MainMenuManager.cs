63f90bbdb90fd62c11823208e238d594e0c617a2
Seth Dossett <95620779+SethDossett@users.noreply.github.com>
2022-11-03 16:14:55 -0400

    menu buttons

--- Assets/_Project/_Scripts/Core/MainMenuManager.cs
+++ Assets/_Project/_Scripts/Core/MainMenuManager.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
-using MyTownProject.SaveLoadSystem;
+using System.Collections;
+using UnityEngine.EventSystems;
 
 public class MainMenuManager : MonoBehaviour
 {
     int lastCurrentScene;
+    [SerializeField] GameObject firstButton;
 
+    private void Awake()
+    {
+        StartCoroutine(SetFirstSelection());
+    }
     public void EnterGame()
     {
         lastCurrentScene = 0;
 
         //SceneController.SwitchScene(lastCurrentScene);
+    }
+    IEnumerator SetFirstSelection()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(firstButton);
+        yield break;
     }
 }
